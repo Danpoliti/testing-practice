4adde662fd286d0eeffb54d8e7964be49913946c
danpoliti <danielcpoliti@yahoo.com.br>
2021-03-28 22:31:52 -0400

    03-product Test Case 4 and 5 successfully executed

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,7 +1,7 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product(num1, num2) {
+function product(num1, num2 = 1) {
   return num1 * num2;
 }
 
@@ -32,12 +32,14 @@ try {
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = product(4);
+  if (result !== 4) throw new Error('Expected product(4) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = product(null);
+  if (result !== 0) throw new Error('Expected product(null) to be 0. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
